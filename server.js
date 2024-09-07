fbc6f0be63acf10d27c7e033a9dc74181a0b2c06
devpatel1729 <146178403+devpatel1729@users.noreply.github.com>
2024-09-07 12:48:08 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,16 @@
 const express = require('express');
-const os = require('os');
-const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// Enable CORS for all origins
+// Use CORS middleware
 app.use(cors());
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Endpoint to get server's local IP addresses
-app.get('/local-ip', (req, res) => {
-  console.log('Received request for /local-ip'); // Log to verify the request
-
-  const interfaces = os.networkInterfaces();
-  const ips = [];
-
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        ips.push(iface.address);
-      }
-    }
-  }
-
-  res.json({ ips });
-});
-
-// Endpoint to get the client's IP address
-app.get('/client-ip', (req, res) => {
-  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.json({ clientIp });
-});
-
-// Serve the frontend
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/get-ip', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.json({ ip });
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
